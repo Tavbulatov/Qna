c5313f13a6da44579e64239b76afa26a07ec3886
Tavbulatov <alikhantam26@gmail.com>
2023-05-30 15:32:51 +0300

    feature test: creating a reward

---
+++ spec/features/question/create_reward_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User creates a reward for the best answer', '
+  when creating a question
+  I, as the author of the question, want to reward the user
+  for the best answer to my question
+' do
+
+  scenario 'create reward', js: true do
+    sign_in(create(:user))
+    click_on 'Ask question'
+    sleep 0.5
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+
+    fill_in 'Reward name', with: 'your award for the best answer'
+    attach_file 'Image', "#{Rails.root}/app/assets/images/test_image.png"
+
+    click_on 'Create Question'
+    sleep 0.5
+
+    click_on 'Test question'
+    sleep 0.5
+    click_on 'Reward'
+
+    expect(page).to have_content("your award for the best answer")
+    expect(page).to have_css("img[src*='test_image.png']")
+  end
+end
