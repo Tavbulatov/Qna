845797719d5b15a11506aa899ce1e7b01c3085ca
Tavbulatov <alikhantam26@gmail.com>
2023-05-30 15:45:01 +0300

    for the model, the question added an association with a reward and set up a function for a nested resource

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,10 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
+  has_one :reward, dependent: :destroy
+
   has_many_attached :files
+  accepts_nested_attributes_for :reward
 
   belongs_to :author, class_name: 'User'
   belongs_to :best_answer, class_name: 'Answer', optional: true
