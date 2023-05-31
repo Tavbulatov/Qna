726f2c942accddf1b9b697712b270958301a730e
Tavbulatov <alikhantam26@gmail.com>
2023-05-31 15:43:40 +0300

    added action index to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
 
-
   resources :attachments, only: :destroy
-  resources :rewards, only: :show
+  resources :rewards, only: %i[show index]
 
   resources :questions do
     resources :answers, shallow: true, only: %i[create update destroy] do
