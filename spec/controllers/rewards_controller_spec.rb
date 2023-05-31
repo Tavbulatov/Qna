0c4cbd8c973e71e489eb65d090f2f4834610e39c
Tavbulatov <alikhantam26@gmail.com>
2023-05-31 15:04:29 +0300

    test rewards contr: test action index

--- spec/controllers/rewards_controller_spec.rb
+++ spec/controllers/rewards_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe RewardsController, type: :controller do
+  let(:question) { create(:question) }
 
   describe "GET #show" do
-    let(:question) { create(:question) }
+
     let(:reward) { create(:reward, question: question)}
 
     before {get :show, params: {id: reward}}
@@ -14,6 +15,24 @@ RSpec.describe RewardsController, type: :controller do
 
     it 'renders show view' do
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #index' do
+    let(:user) { create(:user) }
+    let(:rewards) { create_list(:reward, 3, question: question, user: user)}
+
+    before do
+      login(user)
+      get :index
+    end
+
+    it 'setting @user_rewards variable for all user rewards' do
+      expect(assigns(:user_rewards)).to eq(rewards)
+    end
+
+    it 'render view index' do
+      expect(response).to render_template :index
     end
   end
 end
