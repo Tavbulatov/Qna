8e70f782bfb0342024b64270572ef797b584eb79
Tavbulatov <alikhantam26@gmail.com>
2023-05-31 15:46:54 +0300

    in factories/rewards added image path and changed name

--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :reward do
-    name { "MyString" }
+    name { "MyReward" }
     question { nil }
+
+    after(:build) do |reward|
+      reward.image.attach(io: File.open("#{Rails.root}/app/assets/images/test_image.png"), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
