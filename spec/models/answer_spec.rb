b0d8aa45b98063f226adf146cdedd2465c9ce7ea
Tavbulatov <alikhantam26@gmail.com>
2023-06-21 15:55:53 +0300

    Answer model: checking association with Link model, checking nested attributes (Link) checking association with Link model, checking nested attributes (Link)

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it { should have_many(:links).dependent(:destroy) }
+
+  it { should have_many_attached(:files) }
+
   it { should belong_to(:question) }
   it { should belong_to(:author).class_name('User') }
+
+  it { should accept_nested_attributes_for(:links).allow_destroy(true) }
 
   it { should validate_presence_of(:body) }
 
@@ -10,6 +16,4 @@ RSpec.describe Answer, type: :model do
   let(:answer) { create(:answer, author: user) }
 
   it { expect(answer.user?(user)).to eq(true) }
-
-  it { should have_many_attached(:files) }
 end
