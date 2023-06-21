5098d5e5df38cb2e02eac9f91036f1295bd5f791
Tavbulatov <alikhantam26@gmail.com>
2023-06-21 15:54:53 +0300

    model test Question
    checking association with Link model, checking nested attributes (Link, Reward)

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:links).dependent(:destroy) }
+
   it { should have_one(:reward).dependent(:destroy) }
+
+  it { should have_many_attached(:files) }
 
   it { should belong_to(:author).class_name('User') }
   it { should belong_to(:best_answer).class_name('Answer').optional(true)  }
+
+  it { should accept_nested_attributes_for(:reward).allow_destroy(true) }
+  it { should accept_nested_attributes_for(:links).allow_destroy(true) }
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
@@ -14,6 +21,4 @@ RSpec.describe Question, type: :model do
   let(:question) { create(:question, author: user) }
 
   it { expect(question.user?(user)).to eq(true) }
-
-  it { should have_many_attached(:files) }
 end
