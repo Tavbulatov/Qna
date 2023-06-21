295e2c3e5dba8815d53862cb4e2459bfbfab8cb6
Tavbulatov <alikhantam26@gmail.com>
2023-06-21 16:00:50 +0300

    Reward spec: Validation check name, photo
    checking the association have_one_attached

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Reward, type: :model do
+  it { should have_one_attached(:image) }
+
   it { should belong_to(:question) }
   it { should belong_to(:user).optional(true)  }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:image) }
 end
