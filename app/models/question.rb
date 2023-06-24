57c2af32188dbe77be927785da5ae1cd03e9f596
Tavbulatov <alikhantam26@gmail.com>
2023-06-25 02:43:47 +0300

    moved the association with files to the QuestionAnswer module and connected this module
    and added for nested attributes: allow_destroy: true

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,13 +1,14 @@
 class Question < ApplicationRecord
+  include QuestionAnswer
+
   has_many :answers, dependent: :destroy
 
   has_one :reward, dependent: :destroy
 
-  has_many_attached :files
-  accepts_nested_attributes_for :reward, reject_if: :all_blank
-
   belongs_to :author, class_name: 'User'
   belongs_to :best_answer, class_name: 'Answer', optional: true
+
+  accepts_nested_attributes_for :reward, reject_if: :all_blank, allow_destroy: true
 
   validates :title, :body, presence: true
 end
