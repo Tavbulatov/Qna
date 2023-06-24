033b4cab8529450c04e21a22be4914d66b12b748
Tavbulatov <alikhantam26@gmail.com>
2023-06-25 02:34:28 +0300

    set up factories for the link

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :link do
     name { "MyString" }
-    url { "MyString" }
+    url { "https://google.com" }
     linkable { nil }
+
+    trait :url_gist do
+      url { "https://gist.github.com/Tavbulatov/789ad83d7cf2f60d151b915f9b34a025" }
+    end
   end
 end
