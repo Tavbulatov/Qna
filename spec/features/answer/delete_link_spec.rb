2baa66fc3366b71873d8c81dcab4f42dc4dc821c
Tavbulatov <alikhantam26@gmail.com>
2023-07-27 00:09:40 +0300

    answer link removal test

---
+++ spec/features/answer/delete_link_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'As the creator of the question and links to it
+I want to be able to remove the link I want
+' do
+
+  given(:user) { create(:user) }
+  given(:question) {create(:question, author: user) }
+  given(:answer) {create(:answer, author: user, question: question)}
+  given!(:link) {create(:link, linkable: answer) }
+
+  scenario 'delete link', js: true  do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.answers_all' do
+      click_on 'Delete link'
+    end
+
+    accept_alert 'Are you sure?'
+
+    within '.answers_all' do
+      expect(page).to_not have_link('Google')
+    end
+  end
+end
