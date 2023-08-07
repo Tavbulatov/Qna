598d5078a96fa269d9c027a3f7062e3e6cd82fbe
Tavbulatov <alikhantam26@gmail.com>
2023-08-07 14:08:11 +0300

    transfer of methods associated with a link to a concern Linkable

--- app/models/concerns/linkable.rb
+++ app/models/concerns/linkable.rb
@@ -4,5 +4,17 @@ module Linkable
   included do
     has_many :links, dependent: :destroy, as: :linkable
     accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
+
+    def gists_url
+      gists.pluck('url')
+    end
+
+    def gists_id
+      gists.map(&:id)
+    end
+
+    def gists
+      links.select { |l| l.gist? }
+    end
   end
 end
--- app/models/concerns/question_answer.rb
+++ app/models/concerns/question_answer.rb
@@ -7,17 +7,5 @@ module QuestionAnswer
     def user?(user)
       author == user
     end
-
-    def gists_url
-      gists.pluck('url')
-    end
-
-    def gists_id
-      gists.map(&:id)
-    end
-
-    def gists
-      links.select { |l| l.gist? }
-    end
   end
 end
