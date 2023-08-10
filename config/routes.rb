039decbff99bb17aeb9ef7c2351333b86f18483c
Tavbulatov <alikhantam26@gmail.com>
2023-08-11 02:02:46 +0300

    added voting routes to question and answer

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
 
+  concern :votable do
+    resources :votes, only: %i[create destroy], shallow: true
+  end
+
   resources :attachments, only: :destroy
   resources :rewards, only: %i[show index]
   resources :links, only: :destroy
 
-  resources :questions do
-    resources :answers, shallow: true, only: %i[create update destroy] do
+  resources :questions, concerns: [:votable], defaults: { votable: 'Question' } do
+    resources :answers, shallow: true, only: %i[create update destroy],
+                                       concerns: [:votable], defaults: { votable: 'Answer' } do
       patch 'best', on: :member
     end
   end
