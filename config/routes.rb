6e2b71d6eeddf90f768d90bb621daeaad73b02f4
Tavbulatov <alikhantam26@gmail.com>
2023-09-19 10:11:42 +0300

    add routes for comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,19 @@ Rails.application.routes.draw do
     resources :votes, only: %i[create destroy], shallow: true
   end
 
+  concern :commentable do
+    resources :comments, only: %i[create destroy], shallow: true
+  end
+
   resources :attachments, only: :destroy
   resources :rewards, only: %i[show index]
   resources :links, only: :destroy
 
-  resources :questions, concerns: [:votable], defaults: { votable: 'Question' } do
+  resources :questions, concerns: [:votable, :commentable],
+                        defaults: { votable: 'Question', commentable: 'Question' } do
     resources :answers, shallow: true, only: %i[create update destroy],
-                        concerns: [:votable], defaults: { votable: 'Answer' } do
+                        concerns: [:votable, :commentable],
+                        defaults: { votable: 'Answer', commentable: 'Answer' } do
       patch 'best', on: :member
     end
   end
