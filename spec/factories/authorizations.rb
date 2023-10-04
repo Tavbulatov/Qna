29db9b6a3da04f4c1beebfbdae108cd7187f0afa
Tavbulatov <alikhantam26@gmail.com>
2023-10-04 22:45:34 +0300

    add Devise.frendly_token for factories authorization

--- spec/factories/authorizations.rb
+++ spec/factories/authorizations.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :authorization do
     provider { "vkontakte" }
     uid { "123123" }
-    confirmation_token { nil }
+    confirmation_token { Devise.friendly_token }
     confirmed_at { nil }
     user { nil }
   end
